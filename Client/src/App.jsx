fb95aefa4b6c5cc58137d6be17b046c47278015f
Suryansh Sharma <suryansharmaa@gmail.com>
2025-02-22 22:43:12 +0530

    progress

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,9 +4,11 @@ import AuthRegister from "./pages/auth/Register";
 import { Routes, Route } from "react-router-dom";
 import AuthLayout from "./components/auth/Layout";
 import AdminOrders from "./pages/admin-view/Orders";
+import CheckAuth from "./components/common/CheckAuth";
 import ShoppingHome from "./pages/shopping-view/Home";
 import AdminFeatures from "./pages/admin-view/Features";
 import AdminProducts from "./pages/admin-view/Products";
+import UnAuthPage from "./pages/unauth-page/UnAuthPage";
 import AdminLayout from "./components/admin-view/layout";
 import AdminDashboard from "./pages/admin-view/Dashboard";
 import ShoppingListing from "./pages/shopping-view/Listing";
@@ -15,22 +17,48 @@ import ShoppingLayout from "./components/shopping-view/Layout";
 import ShoppingCheckout from "./pages/shopping-view/Checkout";
 
 function App() {
+  const isAuthenticated = true;
+  const user = {
+    name: "Suryansh",
+    role: "user",
+  };
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       <Routes>
-        <Route path="/auth" element={<AuthLayout />}>
+        <Route
+          path="/auth"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AuthLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="login" element={<AuthLogin />} />
           <Route path="register" element={<AuthRegister />} />
         </Route>
 
-        <Route path="/admin" element={<AdminLayout />}>
+        <Route
+          path="/admin"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AdminLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="dashboard" element={<AdminDashboard />}></Route>
           <Route path="features" element={<AdminFeatures />}></Route>
           <Route path="orders" element={<AdminOrders />}></Route>
           <Route path="products" element={<AdminProducts />}></Route>
         </Route>
 
-        <Route path="/shopping" element={<ShoppingLayout />}>
+        <Route
+          path="/shopping"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <ShoppingLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="account" element={<ShoppingAccount />} />
           <Route path="checkout" element={<ShoppingCheckout />} />
           <Route path="home" element={<ShoppingHome />} />
@@ -38,6 +66,8 @@ function App() {
         </Route>
 
         <Route path="*" element={<NotFound />}></Route>
+
+        <Route path="/unauth-page" element={<UnAuthPage />} />
       </Routes>
     </div>
   );
---
+++ Client/src/pages/unauth-page/UnAuthPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function UnAuthPage() {
+  return (
+    <>
+      <h1>You do not have access to this page</h1>
+    </>
+  );
+}
